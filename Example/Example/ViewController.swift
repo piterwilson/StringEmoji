21425fd20b24c09059ec284c48187193c6e40e80
Juan Carlos Ospina Gonzalez <juan@twcapps.com>
2019-05-14 18:09:39 +0200

    Initial Example View Controller setup

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -9,25 +9,16 @@ import UIKit
 import StringEmoji
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        print("".isEmoji) // false
-//        print("😁".isEmoji) // true
-//        print("😁😜".isEmoji) // false
-//        print("😁😜".containsEmoji) // true
-//        print("á".unicodeName) // \N{LATIN SMALL LETTER A WITH ACUTE}
-//        print("😜".unicodeName) // "\N{FACE WITH STUCK-OUT TONGUE AND WINKING EYE}"
-//        print("😜".niceUnicodeName) // "FACE WITH STUCK-OUT TONGUE AND WINKING EYE"
-//        print("😁😜".unicodeName) // \N{GRINNING FACE WITH SMILING EYES}\N{FACE WITH STUCK-OUT TONGUE AND WINKING EYE}
-//        print("😁😜".niceUnicodeName) // GRINNING FACE WITH SMILING EYES FACE WITH STUCK-OUT TONGUE AND WINKING EYE
-        print("😜".first!.isEmoji) // true
-        print("😜".isEmoji) // true
-        print("012345".containsEmoji) // false
-        print("012345".first!.isEmoji) // false
-        let string = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{};'\\:|,./<>?"
-        for char in string {
-            print("\(char).isEmoji \(char.isEmoji)")
-        }
+        setupTitle()
+    }
+    
+    private func setupTitle() {
+        self.title = "Emoji Unicode List 5.0 "
     }
 }
 
